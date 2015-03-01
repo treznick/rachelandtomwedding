83df2b6b1feb1fcd9f6a8dd6a91efbd8cec1a27a
Tom Reznick <thomas.reznick+treznick@gmail.com>
2015-03-01 18:30:49 -0500

    Further unminify javascript.

--- public/js/system.js
+++ public/js/system.js
@@ -1,10 +1,14 @@
 ! function() {
-  "use strict";
+
   $("#menu-close").click(function(a) {
-    a.preventDefault(), $("#sidebar-wrapper").toggleClass("active")
-  }), $("#menu-toggle").click(function(a) {
-    a.preventDefault(), $("#sidebar-wrapper").toggleClass("active")
-  }), $(function() {
+    a.preventDefault(), $("#sidebar-wrapper").toggleClass("active");
+  })
+
+  $("#menu-toggle").click(function(a) {
+    a.preventDefault(), $("#sidebar-wrapper").toggleClass("active");
+  })
+
+  $(function() {
     $("a[href*=#]:not([href=#])").click(function() {
       if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") || location.hostname == this.hostname) {
         var a = $(this.hash);
@@ -13,11 +17,23 @@
         }, 1e3), !1
       }
     })
-  }), $(window).load(function() {
+  })
+
+  $(window).load(function() {
     $("#loader").fadeOut(), $("#mask").delay(1e3).fadeOut("slow")
-  }), $(document).scroll(function() {
-    $(document).scrollTop() >= 500 ? ($(".navbar").addClass("navbar-fixed-top"), $("html").addClass("has-fixed-nav")) : ($(".navbar").removeClass("navbar-fixed-top"), $("html").removeClass("has-fixed-nav"))
-  }), $(document).ready(function() {
+  })
+
+  $(document).scroll(function() {
+    if ( $(document).scrollTop() >= 500 ) {
+      $(".navbar").addClass("navbar-fixed-top");
+      $("html").addClass("has-fixed-nav");
+    } else {
+      $(".navbar").removeClass("navbar-fixed-top");
+      $("html").removeClass("has-fixed-nav");
+    }
+  })
+
+  $(document).ready(function() {
     $("#tab-container").easytabs()
   })
 }(jQuery);
